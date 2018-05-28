0ae914d45439107e892873cd82df88ad1cd9b5d0
Seyed Majid Azimi <m_azimi@web.de>
2018-05-29 00:02:36 +0200

    Update convert_caffe_to_caffe2.sh

--- convert_caffe_to_caffe2.sh
+++ convert_caffe_to_caffe2.sh
@@ -7,8 +7,6 @@ python -m caffe2.python.caffe_translator /home/majid/Myprojects/CAFFE/models/VGG
 (caffe_2) majid@majid:~/Myprojects/pytorch/compiled/lib/python2.7/site-packages/caffe2/python$ python -m caffe2.python.caffe_translator /home/majid/Myprojects/CAFFE/models/VGG_ILSVRC_16_layers/deploy_v1.prototxt /home/majid/Myprojects/CAFFE/models/VGG_ILSVRC_16_layers/VGG_ILSVRC_16_layers.caffemodel
 
 
-# use convert tool from caffe-tensorflow to convert caffe model to tensorflow format
-python convert.py /data/ken/models/VGG_ILSVRC_16_layers_deploy.prototxt /data/ken/models/VGG_ILSVRC_16_layers.caffemodel /data/ken/models/vgg16_conv.pkl /data/ken/models/vgg16_conv.py
+(fasterrcnn-py2) majid@majid:~/Myprojects/CAFFE/python$ python vgg16.tf/src/dump_caffemodel_weights.py --caffe_root /home/majid/Myprojects/CAFFE --caffemodel_path /home/majid/Myprojects/CAFFE/models/VGG_CNN_M_1024/VGG_CNN_M_1024.caffemodel --prototxt_path /home/majid/Myprojects/CAFFE/models/VGG_CNN_M_1024/deploy.prototxt --caffe_weights_path VGG_CNN_M_1024.pkl
 
-# use dump_network_hdf5 tool from mocha to convert caffe model to hdf5
-./dump_network_hdf5 /data/ken/models/VGG_ILSVRC_16_layers_deploy.prototxt /data/ken/models/VGG_ILSVRC_16_layers.caffemodel /data/ken/models/vgg16_conv.hdf5
+(fasterrcnn-py2) majid@majid:~/Myprojects/CAFFE/python$ python vgg16.tf/src/dump_caffemodel_weights.py --caffe_root /home/majid/Myprojects/CAFFE --caffemodel_path /home/majid/Myprojects/CAFFE/models/VGG_ILSVRC_16_layers/VGG_ILSVRC_16_layers.caffemodel --prototxt_path /home/majid/Myprojects/CAFFE/models/VGG_ILSVRC_16_layers/deploy.prototxt --caffe_weights_path VGG_ILSVRC_16_layers.pkl
